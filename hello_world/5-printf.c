12857d1c1eb33be36883723ad6ff3a4184e6acb7
JCox924 <Joshua.Cox@atlasschool.com>
2024-01-22 18:44:09 +0000

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - printf
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+
+{
+
+printf("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s\n",
+           "with", " ", "proper", " ", "grammar,", " ", "but", " ",
+           "the", " ", "outcome", " ", "is", " ", "a piece of art");
+return (0);
+
+}
