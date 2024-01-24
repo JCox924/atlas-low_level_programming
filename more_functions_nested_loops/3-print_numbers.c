6971a611d74df48989ef8e196b5242711eab8280
JCox924 <Joshua.Cox@atlasschool.com>
2024-01-24 13:53:26 -0600

    Added a file that print all numbers from 0-9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_numbers- Prints numbers 0-9
+ *
+ *
+ *
+ *
+ *
+ *
+*/
+
+void print_numbers(void)
+{
+	int i;
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar(i);
+	}
+	i++
+}
