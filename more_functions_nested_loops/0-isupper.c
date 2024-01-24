37b0ebb6cc7673fc8e0555312c4bbafd3132a221
JCox924 <Joshua.Cox@atlasschool.com>
2024-01-24 13:18:32 -0600

    fixed coding style

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-*main- Checks for upper case letters
-*
-*Return: returns 1 is char is uppercase, 0 otherwise
-*/
+ *main- Checks for upper case letters
+ *
+ *Return: returns 1 is char is uppercase, 0 otherwise
+ */
 
 int _isupper(int c)
 {
-	if ( c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
