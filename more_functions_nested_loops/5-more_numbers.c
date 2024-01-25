74c0b3242af689cee12c226fe741b76ceda40525
JCox924 <Joshua.Cox@atlasschool.com>
2024-01-25 08:53:22 -0600

    added a file that prints the numbers 0-14 10 times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*more_number- Prints numbers 0-14 10x
+*
+*
+*
+*/
+
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		int digit;
+
+		for (digit = 0; digit < 15; digit++)
+		{
+			_putchar(digit + '0');
+		}
+	_putchar('\n');
+	}
+}
