f28e0b567ae9f097a1f1f2618c104805af9d3da1
JCox924 <Joshua.Cox@atlasschool.com>
2024-01-25 08:21:56 -0600

    added a file that print all numbers 0-9 execpt for 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_most_numbers- prints numbers 0-9 execpt for 2 and 4
+*
+*
+/*
+
+void print_most_numbers(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++;)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i);
+		}
+	}
+
+	_putchar('\n');
+
+
+}
