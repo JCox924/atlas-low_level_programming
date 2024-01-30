9482b07ef65c8e91998b877dad62d430866d9448
JCox924 <Joshua.Cox@atlasschool.com>
2024-01-30 05:53:01 +0000

    Added a file that might work idk at this point

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_line - makes a line with subscripts
+ *
+ * @n: number of subscripts
+ *
+ * Return: void
+ */
+
+void print_line(int n);
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			putchar(95);
+		}
+
+		_putchar('\n');
+	}
+}
+
