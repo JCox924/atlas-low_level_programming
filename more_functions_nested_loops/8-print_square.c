682f797c3e033eb6883347861f77f63e7bec7353
JCox924 <Joshua.Cox@atlasschool.com>
2024-01-31 03:01:16 +0000

    added a file that pritns a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *
+ * print_square - prints a square of #'s
+ *
+ * @size: size of square
+ *
+ * Return: new if 0 void if != 0
+ */
+
+void print_sqaure(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	int i , j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(23);
+		}
+
+		_putchar('\n');
+	}
+}
