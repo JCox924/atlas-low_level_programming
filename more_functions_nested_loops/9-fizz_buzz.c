ab2c4aa96e4ba43d67f917788c50715fbf3677ab
JCox924 <Joshua.Cox@atlasschool.com>
2024-01-31 03:55:39 +0000

    added a file that does the fizzbuzz multiples thing

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- print fizzbuzz, fizz, and bizz and multiples
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++;)
+	
+	{
+		if ( i % 3 == 0 && i % 5 == 0)
+
+		{
+			printf("Fizzbuzz");
+		}
+		else if (i % 3 == 0)
+
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		
+		{
+			printf("Buzz");
+		}
+		else
+
+		{
+			printf("%d " , i);
+		}
+	}
+}
+
