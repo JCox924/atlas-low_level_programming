bf717f9da1a616f6fc781e12f791ea15488103af
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-05 23:24:58 +0000

    added a file that swaps values

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main/h"
+
+/**
+ * swap_int - swaps vales of two integers
+ *
+ * @a: pointer a
+ *
+ * @b: pointer b
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
