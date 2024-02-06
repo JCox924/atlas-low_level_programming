c6a62f433952ffe336966778a8ebb6f2a00c9730
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-06 02:32:53 +0000

    added 2 files that essentially do the same thing but the second one has a number spec

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strcat - concatenates 2 strings
+ *
+ * @dest: pointer to destination
+ *
+ * @src: pointer to string
+ *
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, d_len, s_len;
+
+	d_len = _strlen(dest);
+	s_len = _strlen(src);
+
+	for (i = 0; i < s_len; i++)
+	{
+		dest[d_len + i] = src[i];
+	}
+
+	dest[d_len + s_len} = '\0';
+
+	return (dest);
+}
---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strncat - concats a string a certain number of times
+ *
+ * @dest: pointer to destination
+ *
+ * @src: pointer to string
+ *
+ * @n: number of times to concat
+ *
+ * Return: concatenated strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+        int i, d_len, s_len;
+
+        d_len = _strlen(dest);
+        s_len = _strlen(src);
+
+        for (i = 0; i < n && src[i] != '\0'; i++)
+        {
+                dest[d_len + i] = src[i];
+        }
+
+        dest[d_len + s_len} = '\0';
+
+        return (dest);
+}
