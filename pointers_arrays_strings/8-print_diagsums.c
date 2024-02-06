9ab37d69595f2866d855638c1ad1ba1f5f2a47b8
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-06 05:49:56 +0000

    added last two files

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints sum of two numbers on an array
+ *
+ * @a:  pointer to array
+ *
+ * @size: size of array
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0, sum_x = 0, sum_y = 0;
+
+	for (i = 0; i < size * size; i += (size + 1))
+	{
+		sum_x += a[i];
+	}
+
+	for (i = size - 1; i < (size *size) - 1; i += (size - 1))
+	{
+		sum_y += a[i];
+	}
+
+	printf("%d, %d\n", sum_x, sum_y);
+}
