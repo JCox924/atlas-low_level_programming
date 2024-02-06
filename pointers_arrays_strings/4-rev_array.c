bd0cb17d007757798b34792e2075447857503509
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-06 03:25:18 +0000

    added files for 4 and 5

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses array of integers
+ *
+ * @a: pointer to array
+ *
+ * @n: # of indices in array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, rv;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		rv = a[n - i - 1];
+		a[n - i - 1] = a[i];
+		a[i] = rv;
+	}
+}
---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all char's in a string to uppercase
+ *
+ * Return: new uppercase string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+	}
+	
+	return (str);
+}
+
