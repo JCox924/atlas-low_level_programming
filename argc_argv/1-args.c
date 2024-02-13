e6efa5e6a48b2ef76cbe1837eeaa1d62d7c92230
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-13 05:25:37 +0000

    fixed compile error

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,12 +6,10 @@
  *
  * @argc: argument counter
  *
- * @argv: argument array pointer
- *
  * Return: always 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc)
 {
 	printf("%d\n" , argc - 1);
 
