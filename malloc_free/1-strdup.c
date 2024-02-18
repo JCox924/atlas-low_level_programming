f7d6c62d99f0ba093e9c6d496b4bd6ff8dd674b5
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-18 22:36:35 +0000

    added a file for #1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include "mylib.a"
+
+/**
+ * _strdup - Returns a pointer to a copied striung in memory
+ *
+ * @str: pointer to string
+ *
+ * Return: NULL if void, pointer to new string if not
+ */
+
+char *_strdup(char *str)
+{
+	char *new_str;
+
+	int i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	new_str = malloc(sizeof(char) * (_strlen(str) + 1));
+
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (; str[i] != '\0'; i++)
+	{
+		new_str[i] = str[i];
+	}
+
+	new_str[i] = '\0';
+
+	return (new_str);
+
+	free(new_str);
+}
