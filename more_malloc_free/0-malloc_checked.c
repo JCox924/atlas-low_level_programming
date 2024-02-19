3bef699b7c58501bca8801651fdde1a0d34f923d
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-19 22:02:34 +0000

    init commit

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocated memory but exits if NULL
+ *
+ *  @b: size of mem to alloc
+ *
+ *  Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return ptr;
+}
---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,85 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * _strlen - returns the lengths of a string
+ *
+ * @s: pointer to string variable
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	char *i = s;
+
+	while (*s != '\0')
+	{
+		++s;
+	}
+	return (s - i);
+}
+
+
+/**
+ *string_nconcat  - returns a string of 2 concatenated strings
+ *
+ *  @s1: pointer to string 1
+ *
+ *  @s2: pointer to string 2
+ *
+ *  @n: number of bytes to init
+ *
+ *  Return: concatenated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len1, len2;
+	
+	int i, j;
+
+	char *result;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len1 = _strlen(s1);
+
+	len2 = _strlen(s2);
+
+	if (n >= len2)
+	{
+		n = len2;
+	}
+
+	result = malloc((len1 + n + 1) * sizeof(char));
+
+	if (result = NULL)
+	{
+		return(NULL);
+	}
+
+	for (i = 0; i < len1; i++)
+	{
+		result[i] = s1[i];
+	}
+
+	for(j = 0; j < n; j++)
+	{
+		result[i + j] = s2[j];
+	}
+
+	result[i + j] = '\0';
+
+	return (result);
+}
+
