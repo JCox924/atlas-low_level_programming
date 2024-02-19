b115f5babb32eb532bfaf2cf3a2347e3fdb0e248
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-19 22:10:33 +0000

    added more ()'s

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -63,7 +63,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	result = malloc((len1 + n + 1) * sizeof(char));
 
-	if ((result) = NULL)
+	if ((result) = (NULL))
 	{
 		return(NULL);
 	}
