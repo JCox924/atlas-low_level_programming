f4e44b005e0e3c0abbad256f60eaa6f57529bc9c
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-19 19:41:53 +0000

    tried () for NULL comp

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -45,12 +45,12 @@ char *str_concat(char *s1, char *s2)
 
 	size_t j = 0;
 
-	if (s1 == NULL)
+	if (s1 == (NULL))
 	{
 		s1 = "";
 	}
 
-	if (s2 == NULL)
+	if (s2 == (NULL))
 	{
 		s2 = "";
 	}
@@ -58,7 +58,7 @@ char *str_concat(char *s1, char *s2)
 
 	result = (char *)malloc(len_s1 + len_s2 + 1);
 
-	if (result == NULL)
+	if (result == (NULL))
 	{
 		return (NULL);
 	}
