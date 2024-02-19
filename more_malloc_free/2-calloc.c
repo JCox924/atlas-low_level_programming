4f653941feba3ad51c0336d50b9af7644900fe9e
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-19 22:33:38 +0000

    init commit of 2-3

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,10 +2,35 @@
 #include <stdlib.h>
 
 /**
- *  - 
+ * _calloc - allocates memory for an array
  *
- *  @:
+ *  @nmemb:
  *
- *  Return:
+ *  @size:
+ *
+ *  Return: void
  */
 
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	size_t size_all = nmemb * size;
+
+	void *p = malloc(size_all);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (; i < size_all; i++)
+	{
+		*((char *)p + 1) = 0:
+	}
+
+	return (p);
+}
--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -2,10 +2,40 @@
 #include <stdlib.h>
 
 /**
- *  - 
+ *_array_range  - create array of ints
  *
- *  @:
+ *  @min: min num in array
  *
- *  Return:
+ *  @max: max num in array
+ *
+ *  Return: array of ints
  */
 
+int *_array_range(int min, int max)
+{
+	size_t size;
+
+	size_t i = 0;
+
+	int *arr;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	size = (size_t)(max - min + 1);
+
+	arr = (int *)malloc(size * sizeof(int));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (; i < size; i++)
+	{
+		arr[i] = min + (int)i;
+	}
+
+	return (arr);
+}
