09390b406e111213015e7aa67f4da3977a3d9103
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-23 20:34:09 +0000

    init commit for 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include "stdlib.h"
+
+/**
+ * print_dog - prints the elements of a function
+ *
+ * @d: pointer to the structure
+ *
+ * Return: void
+ */
+
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	printf("Name: %s\n", (d -> name != NULL) ? d -> name : "(nil)");
+	printf("Age: %f\n", d-> age);
+	printf("Owner: %s\n", (d-> owner != NULL) ? d-> breed : "(nil)");
+}
