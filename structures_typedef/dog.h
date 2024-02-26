4dcaf542e7eebb75c4849d4c6acb561b72f09fa0
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-26 19:13:22 +0000

    fix1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -21,7 +21,7 @@ typedef struct dog
 } dog_t;
 
 int _strlen(char *s);
-char _strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
