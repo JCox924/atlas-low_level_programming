4a765086a32322e41921100770701a730bcd58da
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-27 03:04:47 +0000

    init commit for #5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stidio.h>
+
+/**
+ * free_dog - Frees the memory allocated to dog_t structs
+ *
+ * @d: poiner to struct
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+
+		if (d->owner != NULL)
+		{
+			free(d->owner);
+		}
+	free(d);
+	}
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -25,5 +25,6 @@ char *_strcpy(char *dest, char *src);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
