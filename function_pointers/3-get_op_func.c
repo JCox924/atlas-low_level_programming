50a429e3fa0560a3253a6753b5906cb3215ec5cf
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-27 06:15:24 +0000

    fix5

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -22,12 +22,13 @@ int (*get_op_func(char *s))(int, int)
     };
     int i = 0;
 
-    for (; i < 5; i++)
+    while (i < 5)
     {
 	    if (strcmp(s, ops[i].op) == 0)
 	    {
 		    return (ops[i].f);
 	    }
+	    i++;
     }
 
 	    return (0);
