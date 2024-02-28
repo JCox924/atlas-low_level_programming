94312f14fa79baf616b9613c3bd11565fa66d8a2
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-28 05:37:31 +0000

    fixed header

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,10 +2,29 @@
 #define _HEADER_
 #include <stdarg.h>
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_a_char(char *separator, va_list args);
+void print_a_integer(char *separator, va_list args);
+void print_a_float(char *separator, va_list args);
+void print_a_char_ptr(char *separator, va_list args);
+
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct f_dt - f_dt struct
+ *
+ * @identifier: conversion specifier
+ *
+ * @f: pointer to function
+ */
 
 typedef struct f_dt
 {
