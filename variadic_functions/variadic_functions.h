25ceabbc7bac25467e61b30c107db11d23b70a35
JCox924 <Joshua.Cox@atlasschool.com>
2024-02-28 05:26:50 +0000

    added stuct to header

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -6,4 +6,9 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+typedef struct format_types
+{
+	char *identifier;
+	void (*f)(char *separator, va_list args);
+} f_dt;
 #endif
