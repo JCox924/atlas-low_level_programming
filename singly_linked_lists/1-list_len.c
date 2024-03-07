09fbae8b6634f7b5131dba46798cb36c53f12e45
JCox924 <Joshua.Cox@atlasschool.com>
2024-03-06 19:53:25 -0600

    init commit for .1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+*list_len - returns the length of a list
+*
+*@h: head
+*
+*Return: length of list
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return counter;
+}
