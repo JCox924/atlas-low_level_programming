2013145305c885bd6ce1ce29049021f08e4af5ee
JCox924 <Joshua.Cox@atlasschool.com>
2024-03-18 18:46:05 -0500

    fixed typo

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -32,7 +32,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	while (current != NULL && i < index)
 	{
-		current = current->next
+		current = current->next;
 		i++;
 	}
 	if (current == NULL)
