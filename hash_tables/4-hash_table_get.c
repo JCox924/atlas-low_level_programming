8b613bcb1c3888e173f6a3d4a706c6c9353e92de
JCox924 <Joshua.Cox@atlasschool.com>
2024-03-18 22:53:00 -0500

    init commit 4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+*hash_table_get - gets value associated with key
+*
+*@ht: hash table
+*
+*@key: key specifier
+*
+*Return: value associated with key, NULL if error
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+	{
+		return (NULL);
+	}
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0);
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+	return (NULL);
+}
+
+
