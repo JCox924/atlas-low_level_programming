12a2706cdf46ec769bc0a2d2555e3fecc89ef428
JCox924 <Joshua.Cox@atlasschool.com>
2024-03-18 21:51:20 -0500

    init commit for 2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
+
+/**
+*key_index - gives index of key
+*
+*@key: key specifier
+*
+*@size: size of array of hash table
+*
+*Return: index key/value pair in array of hash table
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	hash = hash_djb2(key);
+
+	return (hash % size);
+}
